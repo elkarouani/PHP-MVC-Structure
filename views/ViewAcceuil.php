cbd80aea58f264747053212bdb193c6b2836cdf1
KDragon <elkarouani@gmail.com>
2019-01-13 16:10:12 +0100

    Fix: editing .htaccess file

--- views/ViewAcceuil.php
+++ views/ViewAcceuil.php
@@ -2,7 +2,7 @@
 <div class="card-body list-group" style="margin-left: 20px;">
 	<?php foreach ($articles as $article) : ?>
 		<div class="list-group-item list-group-item-action">
-			<a href="index.php?url=articles/<?= $article->id(); ?>" class="card-link">
+			<a href="articles/<?= $article->id(); ?>" class="card-link">
 				<?= $article->title(); ?>
 			</a>
 			<p class="card-text"><time><?= $article->date(); ?></time></p>
